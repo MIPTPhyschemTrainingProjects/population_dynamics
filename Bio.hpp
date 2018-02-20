24e3e831ce9c813f50ce4bf71c0295dae9841817
Vladimir Shadymov <vladimir.shadymov@gmail.com>
2018-02-20 23:06:36 +0300

    Fisrt step it making creatures is done

---
+++ Bio.hpp
@@ -0,0 +1,21 @@
+//
+// Created by vladimir on 20.02.18.
+//
+
+#ifndef POPULATION_DYNAMICS_BIO_HPP
+#define POPULATION_DYNAMICS_BIO_HPP
+
+
+
+class Bio{
+ private:
+  long int lifetime; /// time of life
+  //TODO other attributes
+ public:
+  //TODO: main virtual methods
+
+};
+
+
+
+#endif //POPULATION_DYNAMICS_BIO_HPP
